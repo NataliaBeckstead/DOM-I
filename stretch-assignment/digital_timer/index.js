4150e60a83c6c80ac7c9654cd08cb3a18d4ffa51
Natalia Beckstead <toosmiravilla@gmail.com>
2020-02-10 16:10:38 -0700

    finally timer shows correctly on page

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -3,8 +3,9 @@ var interval;
 function timer() {  
    interval = setInterval(function() {
         //printing time on screen
-        let intString = miliseconds.toString();
-        if (interval < 10) {
+        let intString = Array.from(miliseconds.toString());
+        console.log(intString);
+        if (intString.length < 2) {
             const st = document.querySelector("#secondTens");
             st.textContent = '0';
             const so = document.querySelector("#secondOnes");
@@ -12,37 +13,34 @@ function timer() {
             const mh = document.querySelector("#msHundreds");
             mh.textContent = '0';
             const mt = document.querySelector("#msTens");
-            mt.textContent = intString.charAt(0);
-        }
-        if (interval < 100) {
+            mt.textContent = intString[0];
+        } else if (intString.length < 3) {
             const st = document.querySelector("#secondTens");
             st.textContent = '0';
             const so = document.querySelector("#secondOnes");
             so.textContent = '0';
             const mh = document.querySelector("#msHundreds");
-            mh.textContent = intString.charAt(0);
+            mh.textContent = intString[0];
             const mt = document.querySelector("#msTens");
-            mt.textContent = intString.charAt(1);
-        }
-        if (interval < 1000) {
+            mt.textContent = intString[1];
+        } else if (intString.length < 4) {
             const st = document.querySelector("#secondTens");
             st.textContent = '0';
             const so = document.querySelector("#secondOnes");
-            so.textContent = intString.charAt(0);
+            so.textContent = intString[0];
             const mh = document.querySelector("#msHundreds");
-            mh.textContent = intString.charAt(1);
+            mh.textContent = intString[1];
             const mt = document.querySelector("#msTens");
-            mt.textContent = intString.charAt(2);
-        }
-        if (interval < 10000) {
+            mt.textContent = intString[2];
+        } else if (intString.length < 5) {
             const st = document.querySelector("#secondTens");
-            st.textContent = intString.charAt(0);
+            st.textContent = intString[0];
             const so = document.querySelector("#secondOnes");
-            so.textContent = intString.charAt(1);
+            so.textContent = intString[1];
             const mh = document.querySelector("#msHundreds");
-            mh.textContent = intString.charAt(2);
+            mh.textContent = intString[2];
             const mt = document.querySelector("#msTens");
-            mt.textContent = intString.charAt(3);
+            mt.textContent = intString[3];
         }
        
         //increasing time
